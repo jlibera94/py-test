26c5cd6d249578584441ea72f9d0f42ae8320939
jlibera94 <jlibera94@hotmail.com>
2024-12-30 12:19:46 -0500

    addition commit

---
+++ simple-test.py
@@ -0,0 +1,10 @@
+class Solution:
+    def add(self, x: int, y: int) -> int:
+        return x + y
+
+if __name__ == "__main__":
+    solution = Solution()
+    z = solution.add(2,3)
+    print(z)
+    
+    
