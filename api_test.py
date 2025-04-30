f1285a22532444222bae0cc954992c87bb5c070f
jlibera94 <jlibera94@hotmail.com>
2025-04-29 20:44:01 -0400

    testing api

--- api_test.py
+++ api_test.py
@@ -2,9 +2,13 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/hello', methods=['GET'])
-def say_hello():
-    return jsonify(message="Hello, world!")
+@app.route('/')
+def home():
+    return jsonify({'message': 'Hello, Flask!'})
+
+@app.route('/status')
+def status():
+    return jsonify({'status': 'Running', 'uptime': '99.99%'})
 
 if __name__ == '__main__':
     app.run(debug=True)
